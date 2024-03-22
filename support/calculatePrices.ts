6643d4eb2772d23b65837dc52507807699169926
notNullThen <epekaryuk@outlook.com>
2024-03-22 19:49:50 +0100

    calculatePrices() description change

--- support/calculatePrices.ts
+++ support/calculatePrices.ts
@@ -3,13 +3,13 @@ import getPriceValue from "./getPriceValue";
 /**
  * Calculates the 8% tax amount and total price including tax based on the input price.
  * @param price The input price as a number, string, or null. If it's a string, it should include a dollar sign.
- * @returns An object containing formatted strings of the tax amount and the total price including tax.
+ * @returns An object containing number values of the tax amount and the total price including tax.
  *
  * @example
- * const { taxPriceFormatted, priceWithTaxFormatted } = calculatePrices(100)
+ * const { taxPrice, priceWithTax } = calculatePrices(100)
  *
- * taxPriceFormatted = "$8.00"
- * priceWithTaxFormatted = "$108.00"
+ * taxPrice = 8
+ * priceWithTax = 108
  */
 export default function (price: number | string | null) {
   let priceNum: number;
